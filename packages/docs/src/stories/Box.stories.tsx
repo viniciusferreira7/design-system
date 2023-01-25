cd8f9fc66e81ec7a3657506d02fcbfeab7acef8e
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-01-25 20:12:00 -0300

    Componente: Box

---
+++ packages/docs/src/stories/Box.stories.tsx
@@ -0,0 +1,17 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Box, BoxProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Surfaces/Box',
+  component: Box,
+
+  args: {
+    children: (
+      <>
+        <span>Testando o elemento box</span>
+      </>
+    ),
+  },
+} as Meta<BoxProps>
+
+export const Primary: StoryObj<BoxProps> = {}
