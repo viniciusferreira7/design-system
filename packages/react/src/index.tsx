d4c5b829616ce79ebb3bdc890a363c48dfa2cad7
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-04-03 19:46:51 -0300

    build: Configuração do Stitches

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,5 +1,9 @@
 import { colors } from '@aquino-ui/tokens'
+import { styled } from './styles'
 
-export function App() {
-  return <h1 style={{ color: colors.ignite300 }}>Hello World</h1>
-}
+export const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite500',
+  borderRadius: '$md',
+  padding: '$4',
+})
