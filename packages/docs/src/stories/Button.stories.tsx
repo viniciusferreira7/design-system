88014f00250e7f966f267994773ebc4cabd7d3c9
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-04-03 22:27:18 -0300

    style: story:Button

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,21 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Button, ButtonProps } from '@aquino-ui/react'
+
+export default {
+  title: 'Button',
+  component: Button,
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Enviar',
+    size: 'big',
+  },
+}
+
+export const Secondary: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Enviar',
+    size: 'big',
+  },
+}
--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,9 +1,29 @@
-import { colors } from '@aquino-ui/tokens'
+import type { ComponentProps } from 'react'
 import { styled } from './styles'
 
 export const Button = styled('button', {
   fontFamily: '$default',
-  backgroundColor: '$ignite500',
+  backgroundColor: '$ignite300',
   borderRadius: '$md',
+  border: '0',
   padding: '$4',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4 ',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'big',
+  },
 })
+
+export type ButtonProps = ComponentProps<typeof Button>
