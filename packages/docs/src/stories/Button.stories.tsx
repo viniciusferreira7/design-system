74bc5b951bcd374e06d9849c33920d3e51e5f8c2
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-04-20 22:37:17 -0300

    docs: Documentação de tokens

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -2,7 +2,7 @@ import type { StoryObj, Meta } from '@storybook/react'
 import { Button, ButtonProps } from '@aquino-ui/react'
 
 export default {
-  title: 'Button',
+  title: 'Form/Button',
   component: Button,
 } as Meta<ButtonProps>
 
